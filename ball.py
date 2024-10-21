0adeeee0109db3d50d33ab24f546502eee028bef
kayan9896 <90473306+kayan9896@users.noreply.github.com>
2024-10-20 21:14:12 -0700

    Create ball.py

---
+++ ball.py
@@ -0,0 +1,92 @@
+import pygame
+import math
+
+class Ball:
+    def __init__(self, x, y, radius, color, width, height, edge_width, pocket_radius, offset, acceleration):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.speed_x = 0.0
+        self.speed_y = 0.0
+        self.acceleration_x = acceleration
+        self.acceleration_y = acceleration
+        self.width = width
+        self.height = height
+        self.edge_width = edge_width
+        self.pocket_radius = pocket_radius
+        self.offset = offset
+        self.buffer_height = offset
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
+
+    def move(self):
+        # Update speed based on acceleration
+        self.speed_x = self.update_speed(self.speed_x, self.acceleration_x)
+        self.speed_y = self.update_speed(self.speed_y, self.acceleration_y)
+
+        self.x += self.speed_x
+        self.y += self.speed_y
+
+
+        # Check for collisions with buffers
+        if self.x < self.edge_width + self.buffer_height + self.radius:
+            if self.y < self.edge_width + self.buffer_height - self.offset or self.y > self.height - self.edge_width - self.buffer_height + self.offset:
+                # Rebound on the leg of the trapezoid
+                self.x = 2 * (self.edge_width + self.buffer_height + self.radius) - self.x
+                self.y = 2 * (self.edge_width + self.buffer_height + self.radius) * (self.y - self.edge_width) / (self.height - 2 * self.edge_width) - self.y
+                self.speed_x, self.speed_y = -self.speed_y, -self.speed_x
+            else:
+                # Rebound on the base of the trapezoid
+                self.x = self.edge_width + self.buffer_height + self.radius
+                self.speed_x = -self.speed_x
+                                
+        elif self.x > self.width - self.edge_width - self.buffer_height - self.radius:
+            if self.y < self.edge_width + self.buffer_height - self.offset or self.y > self.height - self.edge_width - self.buffer_height + self.offset:
+                # Rebound on the leg of the trapezoid
+                self.x = 2 * (self.width - self.edge_width - self.buffer_height - self.radius) - self.x
+                self.y = 2 * (self.height - self.edge_width - self.buffer_height - self.radius) * (self.y - self.edge_width) / (self.height - 2 * self.edge_width) - self.y
+                self.speed_x, self.speed_y = self.speed_y, self.speed_x
+            else:
+                # Rebound on the base of the trapezoid
+                self.x = self.width - self.edge_width - self.buffer_height - self.radius
+                self.speed_x = -self.speed_x
+
+        if self.y < self.edge_width + self.buffer_height + self.radius:
+            self.y = self.edge_width + self.buffer_height + self.radius
+            self.speed_y = -self.speed_y
+        elif self.y > self.height - self.edge_width - self.buffer_height - self.radius:
+            self.y = self.height - self.edge_width - self.buffer_height - self.radius
+            self.speed_y = -self.speed_y
+
+        # Check for pockets
+        if self.check_pocket(self.edge_width + self.offset, self.edge_width + self.offset, self.pocket_radius):
+            self.reset()
+        elif self.check_pocket(self.width - self.edge_width - self.offset, self.edge_width + self.offset, self.pocket_radius):
+            self.reset()
+        elif self.check_pocket(self.edge_width + self.offset, self.height - self.edge_width - self.offset, self.pocket_radius):
+            self.reset()
+        elif self.check_pocket(self.width - self.edge_width - self.offset, self.height - self.edge_width - self.offset, self.pocket_radius):
+            self.reset()
+        elif self.check_pocket(self.width // 2, self.edge_width, self.pocket_radius):
+            self.reset()
+        elif self.check_pocket(self.width // 2, self.height - self.edge_width, self.pocket_radius):
+            self.reset()
+
+    def check_pocket(self, x, y, radius):
+        return (self.x - x) ** 2 + (self.y - y) ** 2 <= radius ** 2
+
+    def update_speed(self, speed, acceleration):
+        if speed > 0:
+            return max(0, speed - acceleration)
+        elif speed < 0:
+            return min(0, speed + acceleration)
+        else:
+            return 0
+            
+    def reset(self):
+        self.x = self.edge_width + (self.width - self.edge_width * 2 - self.pocket_radius * 2) / 8 * 2
+        self.y = self.edge_width + self.offset
+        self.speed_x = 0
+        self.speed_y = 0
